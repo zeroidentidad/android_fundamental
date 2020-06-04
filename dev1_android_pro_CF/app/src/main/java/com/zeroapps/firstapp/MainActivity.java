1d0b81446ef6e85ff8e9cd2aa73eb34f238b527e
zeroidentidad <jafs-2050@hotmail.com>
2020-06-04 13:13:38 -0500

    android cf
    logcat test

--- dev1_android_pro_CF/app/src/main/java/com/zeroapps/firstapp/MainActivity.java
+++ dev1_android_pro_CF/app/src/main/java/com/zeroapps/firstapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.zeroapps.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test();
+    }
+
+    public void  test(){
+        Log.e("test e", "kepedo e :v");
+        Log.w("test w", "kepedo w :v");
+        Log.i("test i", "kepedo i :v");
+        Log.d("test d", "kepedo d :v");
+        Log.v("test d", "kepedo v :v");
     }
 }
