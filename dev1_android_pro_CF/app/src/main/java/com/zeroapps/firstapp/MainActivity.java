bbe7ab2e74f27373b42e23bb300d82796191ddad
zeroidentidad <jafs-2050@hotmail.com>
2020-06-05 17:32:02 -0500

    android cf
    params diseño

--- dev1_android_pro_CF/app/src/main/java/com/zeroapps/firstapp/MainActivity.java
+++ dev1_android_pro_CF/app/src/main/java/com/zeroapps/firstapp/MainActivity.java
@@ -1,15 +1,30 @@
 package com.zeroapps.firstapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        LinearLayout linearLayout=new LinearLayout(getApplicationContext());
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        //setContentView(R.layout.activity_main);
+        linearLayout.setBackgroundColor(Color.GREEN);
+        setContentView(linearLayout);
+
+        LinearLayout.LayoutParams params= new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        TextView textView=new TextView(getApplicationContext());
+        textView.setText("kepedo prro");
+        //textView.setLayoutParams(params);
+        textView.setBackgroundColor(Color.RED);
+        linearLayout.addView(textView, params);
         test();
     }
 
