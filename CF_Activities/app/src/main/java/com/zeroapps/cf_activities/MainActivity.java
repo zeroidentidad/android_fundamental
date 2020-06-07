261ddc9b619b42a251747b5e2475922509037e1c
zeroidentidad <jafs-2050@hotmail.com>
2020-06-07 17:02:25 -0500

    android cf
    ciclo vida activity

--- CF_Activities/app/src/main/java/com/zeroapps/cf_activities/MainActivity.java
+++ CF_Activities/app/src/main/java/com/zeroapps/cf_activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"onCreate",Toast.LENGTH_LONG).show();
         btnShow=(Button) findViewById(R.id.btnShow);
         btnShow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +24,35 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(),"onStart",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Toast.makeText(getApplicationContext(),"onPostResume",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(),"onPause",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(),"onStop",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(),"onDestroy",Toast.LENGTH_LONG).show();
     }
 }
--- CF_Activities/app/src/main/java/com/zeroapps/cf_activities/MainActivity2.java
+++ CF_Activities/app/src/main/java/com/zeroapps/cf_activities/MainActivity2.java
@@ -2,6 +2,7 @@ package com.zeroapps.cf_activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(),"onDestroy",Toast.LENGTH_LONG).show();
     }
 }
