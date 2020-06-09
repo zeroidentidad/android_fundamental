a09857a016b90f9f581b8aad2a47c9f2cfc11da9
zeroidentidad <jafs-2050@hotmail.com>
2020-06-08 20:16:58 -0500

    android cf
    orientacion restore data instance

---
+++ CF_Orientacion/app/src/main/java/com/zeroapps/cf_orientacion/MainActivity.java
@@ -0,0 +1,33 @@
+package com.zeroapps.cf_orientacion;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("name","Jesus");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Toast.makeText(getApplicationContext(),"Nombre "+savedInstanceState.getString("name",""),Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(),"onDestroy",Toast.LENGTH_LONG).show();
+    }
+}
