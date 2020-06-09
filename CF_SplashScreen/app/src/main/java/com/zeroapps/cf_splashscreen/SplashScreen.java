6c0b1d919ef0968b308385b17914a0e72ef75146
zeroidentidad <jafs-2050@hotmail.com>
2020-06-08 19:36:12 -0500

    android cf
    splash screen

---
+++ CF_SplashScreen/app/src/main/java/com/zeroapps/cf_splashscreen/SplashScreen.java
@@ -0,0 +1,18 @@
+package com.zeroapps.cf_splashscreen;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
