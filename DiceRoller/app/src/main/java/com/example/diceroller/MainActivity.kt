1bd7b242ea2f5fbbcebb3708950cf5820be566a9
zeroidentidad <jafs-2050@hotmail.com>
2022-08-01 21:30:07 -0500

    android exercise

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.diceroller
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,10 +12,20 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
 
         rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_LONG)//.show()
-            toast.show()
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "6 culos"
+            rollDice()
         }
     }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+   fun roll(): Int {
+       return (1..numSides).random()
+   }
 }
